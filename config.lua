af737799888072d1c0e3b9413b55fc447876ce94
SMDX Development <emilsven2021@gmail.com>
2024-09-19 11:55:14 +0200

    Update config.lua
    Made the code layout somewhat less messy.

--- config.lua
+++ config.lua
@@ -2,5 +2,5 @@ Config = {}
 
 Config.Blips = {
     ["1"] = {name = 'Hospital', coords = vector3(210.27, -325.4, 44.03), sprite = 38, scale = 0.8, color = 2, shortRange = true, enabled = true},
-    -- Add more here
+    -- Add more here if you want
 }
